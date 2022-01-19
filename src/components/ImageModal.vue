020d475af1d1b9935f8d71bac36ddddb8ebdd329
rock-zion <rock_zion@outlook.com>
2022-01-19 23:31:56 +0100

    Update ImageModal.vue

--- src/components/ImageModal.vue
+++ src/components/ImageModal.vue
@@ -1,4 +1,6 @@
 <template>
+  <div @click.self="toggleModal" class="modal_overlay">
+    <div class="modal_content">
       <div
         v-if="imageObject?.height > imageObject?.width"
         class="portrait_wrapper"
@@ -40,6 +42,32 @@
 </script>
 <style lang="scss" scoped>
   .modal_overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba($color: #000000, $alpha: 0.4);
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+
+  .modal_content {
+    width: 80%;
+    max-width: 1000px;
+    height: 90vh;
+    max-height: 800px;
+    background: #ffffff;
+    border-radius: 16px 16px 0 0;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
   .portrait_wrapper {
     height: 80%;
     width: 100%;
