33f15098655ea29915b300295ec622ee8d2def3b
rock-zion <rock_zion@outlook.com>
2022-01-19 23:52:12 +0100

    render details of user

--- src/components/ProfileImage.vue
+++ src/components/ProfileImage.vue
@@ -1,4 +1,13 @@
 <template>
+  <div class="wrapper">
+    <div class="img_wrapper">
+      <img :src="img" />
+    </div>
+    <div class="name_wrapper">
+      <span>{{ name }}</span>
+      <span>{{ location }}</span>
+    </div>
+  </div>
 </template>
 <script>
   export default {
