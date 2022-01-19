eda4a3a41ad52859e95c760bf931aff02aa73df9
rock-zion <rock_zion@outlook.com>
2022-01-19 23:36:58 +0100

    toggle modal visibility

--- src/components/PictureThumbnail.vue
+++ src/components/PictureThumbnail.vue
@@ -4,6 +4,12 @@
 <script>
   export default {
     name: 'PictureThumbnail',
+    methods: {
+      toggleModal(data) {
+        this.$store.commit('updateModal');
+        this.$store.commit('updateSingleImageData',data);
+      },
+    },
   };
 </script>
 
