bf8aebdc16e1acbbcd9733963f4b0a9606dcd3aa
rock-zion <rock_zion@outlook.com>
2022-01-19 23:34:52 +0100

    style header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,4 +14,14 @@
   };
 </script>
 <style lang="scss" scoped>
+  .header {
+    width: 100vw;
+    height: 300px;
+    background-image: url(https://res.cloudinary.com/rock-zion/image/upload/v1642445678/paul-zoetemeijer-ekBOf6sJYYo-unsplash-min.jpg);
+    background-position: top;
+    background-size: 100% auto;
+    background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+  }
 </style>
