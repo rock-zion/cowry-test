7f6d3cc65b92eb4180718184af205ea46145ea9b
rock-zion <rock_zion@outlook.com>
2022-01-19 23:21:11 +0100

    add modal mount and unmount transition

--- src/App.vue
+++ src/App.vue
@@ -62,4 +62,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
 </style>
