45c26909c46565bf0b29f8858945a01b605fd5fe
rock-zion <rock_zion@outlook.com>
2022-01-19 23:47:37 +0100

    optionally render search or loading indicator

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,14 @@
 <template>
   <form @submit.prevent="handleSubmit" class="search_form">
     <input
+      v-if="!$store.state.loading"
       v-model="searchValue"
       type="text"
       placeholder="enter search query here"
     />
+    <h1 class="searching_indicator" v-else>
+      Searching for <span> "{{ $store.state.searchValue }}" </span>
+    </h1>
   </form>
 </template>
 
