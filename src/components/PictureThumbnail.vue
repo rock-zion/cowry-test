4381432bd702b9e6f041374112a35515240f11af
rock-zion <rock_zion@outlook.com>
2022-01-20 00:00:44 +0100

    Update PictureThumbnail.vue

--- src/components/PictureThumbnail.vue
+++ src/components/PictureThumbnail.vue
@@ -28,4 +28,49 @@
 </script>
 
 <style lang="scss" scoped>
+  .img_wrapper {
+    position: relative;
+    width: 100%;
+    margin: 0 1rem 1rem 0;
+    display: inline-block;
+    height: auto;
+  }
+
+  .img_wrapper img {
+    width: 100%;
+    border-radius: 8px;
+    height: auto;
+  }
+
+  .name_wrapper {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 3px;
+    border-radius: 8px;
+    z-index: 100;
+    color: white;
+    background: rgba($color: #000000, $alpha: 0.2);
+    display: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    opacity: 0;
+    transition: all 1;
+  }
+
+  .img_wrapper:hover > .name_wrapper {
+    opacity: 1;
+  }
+
+  .name_wrapper > span {
+    margin-left: 24px;
+    font-weight: 700;
+  }
+
+  .name_wrapper > span:last-child {
+    margin-bottom: 24px;
+    font-size: 12px;
+  }
 </style>
