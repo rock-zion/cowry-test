147a5fa4a91fa2562f052f0e09218422d27ca539
rock-zion <rock_zion@outlook.com>
2022-01-19 23:41:31 +0100

    conditionally render image thumbnails or loading shimmers

--- src/components/GalleryContainer.vue
+++ src/components/GalleryContainer.vue
@@ -1,4 +1,21 @@
 <template>
+  <div v-if="$store.state.imageData.length !== 0" class="container">
+    <PictureThumbnail
+      v-for="item in $store.state.imageData"
+      v-bind:key="item?.id"
+      :name="item?.user?.name"
+      :location="item?.user?.location"
+      :description="item?.alt_description"
+      :img="item?.urls?.thumb"
+      :data="item"
+    />
+  </div>
+
+  <div v-if="$store.state.loading" class="loading_wrapper">
+    <ThumbnailLoading />
+    <ThumbnailLoading />
+    <ThumbnailLoading />
+  </div>
 </template>
 
 <script>
