b5d809eba14d254d21cf7d9386bf06b1393a6332
rock-zion <rock_zion@outlook.com>
2022-01-19 23:37:40 +0100

    add default vuex state

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    imageData: [],
+    singleImageData: {},
+    per_page: 8,
+    page: 1,
+    errors: [],
+    loading: false,
+    searchValue: 'African',
+    displayModal: false,
   },
   mutations: {
   },
