6aff03563021dbb0ac55caece1f3481e2786b0fe
rock-zion <rock_zion@outlook.com>
2022-01-19 23:34:31 +0100

    added search input component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,16 @@
 <template>
+  <div class="header">
+    <Search />
+  </div>
 </template>
 <script>
+  import Search from './Search.vue';
 
   export default {
     name: 'Header',
+    components: {
+      Search,
+    },
   };
 </script>
 <style lang="scss" scoped>
